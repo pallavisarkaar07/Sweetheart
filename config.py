e83317103f750625c6c2b5245ec75f94a5ddf97c
pallavisarkaar07 <vn350h@gmail.com>
2025-10-13 01:42:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "7710642242"))
+OWNER_ID = int(getenv("OWNER_ID", "8457570997"))
 
 LOG = int(getenv("LOG", True))
 
@@ -41,15 +41,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vishumusic/VishuMusicBot",
+    "https://github.com/pallavisarkaar07/Sweetheart",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tabahi_tabahi")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tabahi_tabahi")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/pallavisarkaar")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mafiamusic07")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
