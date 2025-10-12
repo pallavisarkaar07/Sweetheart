a19d333a2fafbcc38d088a8e357e0f74b603e6b3
pallavisarkaar07 <vn350h@gmail.com>
2025-10-13 01:53:11 +0530

    Update extras.py

--- SmartMusic/utils/inline/extras.py
+++ SmartMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="🍁ꜱᴜᴘᴘᴏʀᴛ🍁", url=f"https://t.me/tabahi_tabahi"
+                text="❤️ꜱᴜᴘᴘᴏʀᴛ❤️", url=f"https://t.me/pallavisarkaar"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
